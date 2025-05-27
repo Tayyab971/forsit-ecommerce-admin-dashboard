170b1cb1445dddb48f79548c83b5748beb057fe9
tayyab971 <tayyabejaz971@gmail.com>
2025-05-28 02:59:52 +0500

    changed one date layout style change and readme updated again.

--- components/Confirmation/Confirmation.vue
+++ components/Confirmation/Confirmation.vue
@@ -10,7 +10,7 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from '@/components/ui/alert-dialog'
-import { Button } from '@/components/ui/button'
+
 import { ref, defineProps, defineEmits, watch } from 'vue'
 
 const props = defineProps<{
